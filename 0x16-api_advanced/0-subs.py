6eb136141397e8bab6caf4839bb52aeb9f5b9db3
Anashm24 <anashoumaid19@gmail.com>
2024-05-10 18:55:08 +0100

    debuggin

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,11 @@
-#!/usr/bin/python
-"""retrieve data from an API endpoint"""
+#!/usr/bin/python3
+"""retrieve data from an api endpoit"""
 import requests
-
-
-def get_subreddit_names():
-    url = "https://www.reddit.com/subreddits.json"
-    headers = {"User-Agent": "MyRedditApp/1.0 by MyUsername"}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subreddit_names = [sub["data"]["display_name"]
-                           for sub in data["data"]["children"]]
-        return sorted(subreddit_names)
-    else:
-        return []
 
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditApp/1.0 by MyUsername"}
+    headers = {"User-Agent": "NyReddutApp/1.0 by MyUsername"}
 
     response = requests.get(url, headers=headers)
 
@@ -29,3 +14,4 @@ def number_of_subscribers(subreddit):
         return data["data"]["subscribers"]
     else:
         return 0
+
