455a358f483a5f96b4afc94d6c88dcfae195abb6
Anashm24 <anashoumaid19@gmail.com>
2024-05-10 18:56:16 +0100

    debuggin

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""retrieve data from an api endpoit"""
+"""Returns the number of subscribers of a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "NyReddutApp/1.0 by MyUsername"}
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    if response.status_code != 200:
         return 0
 
+    data = response.json()['data']
+
+    return data['subscribers']
